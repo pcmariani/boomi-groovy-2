10eae8a4222f555e19638e8d9bc018bfd02c66e0
Pete Mariani <pcmariani@gmail.com>
2023-12-20 11:16:56 -0500

    initial commit

---
+++ boomi.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+nameOfBoomiGroovyScript="boomi2.groovy"
+
+workingDir="$(pwd)"
+# echo "workdingDir: $workingDir"
+# echo "BOOMI_GROOVY_HOME: $BOOMI_GROOVY_HOME"
+
+pushd $BOOMI_GROOVY_HOME >/dev/null
+
+groovy "$BOOMI_GROOVY_HOME"/"$nameOfBoomiGroovyScript" $@ -w "$workingDir"
+exitCode="$?"
+
+popd >/dev/null
+
+if [[ "$exitCode" != "0" ]]; then
+	exit 1
+fi
