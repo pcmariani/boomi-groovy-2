93d34212894b2dfd13d1f590d3d60473f2eab6f7
Pete Mariani <pcmariani@gmail.com>
2024-01-17 12:16:38 -0500

    works now

--- src/boomiScriptRun.groovy
+++ src/boomiScriptRun.groovy
@@ -1,17 +1,18 @@
 class boomiScriptRun {
   static void main(String[] args) throws Exception {
 
-    def cli = new CliBuilder(usage: 'boomiScriptTest [-h] [testSuiteFileName]')
+    def cli = new CliBuilder(usage: 'boomiScriptRun [-h] [testSuiteFileName]'
+      // + ' | \n[-h] [-s script] [-d data] [-p properties] [-xp] [-xd]'
+    )
 
     cli.with {
       h  longOpt: 'help', 'Show usage'
-      w  longOpt: 'working-dir', args: 1, argName: 'dir', 'Present Working Directory'
-      // t  longOpt: 'test-suite-file', args: 1, argName: 'test-suite-file', 'Test Suite File'
-      s  longOpt: 'script', args: 1, argName: 'script', 'If not using a testsuite file: Script Filename'
-      d  longOpt: 'document', args: 1, argName: 'document', 'If not using a testsuite file: Document Filename'
-      p  longOpt: 'properties', args: 1, argName: 'properties', 'If not using a testsuite file: Properties Filename'
-      xd longOpt: 'suppress-data-output', type: boolean, 'Suppress data output (can also be done inside OPTIONS in a testsuite file)'
-      xp longOpt: 'suppress-props-output', type: boolean, 'Suppresses props output (can also be done inside OPTIONS in a testsuite file)'
+      w  longOpt: 'workingDir', args: 1, argName: 'dir', 'Present Working Directory'
+      // s  longOpt: 'script', args: 1, argName: 'script', 'If not using a testsuite file: Script Filename'
+      // d  longOpt: 'document', args: 1, argName: 'document', 'If not using a testsuite file: Document Filename'
+      // p  longOpt: 'properties', args: 1, argName: 'properties', 'If not using a testsuite file: Properties Filename'
+      // xd longOpt: 'suppress-data-output', type: boolean, 'Suppress data output (can also be done inside OPTIONS in a testsuite file)'
+      // xp longOpt: 'suppress-props-output', type: boolean, 'Suppresses props output (can also be done inside OPTIONS in a testsuite file)'
     }
 
     def options = cli.parse(args)
@@ -21,23 +22,17 @@ class boomiScriptRun {
       return
     }
 
-    def workingDir = options.workingDir ?: System.getProperty("user.dir")
-    def testSuiteFileName = options.arguments()[0]
-
+    String testSuiteFileName = options.arguments()[0]
     GlobalOptions.workingDir = options.workingDir ?: System.getProperty("user.dir")
     GlobalOptions.mode = "run"
 
-    // TestSuiteRunner testSuiteRunner = new TestSuiteRunner(GlobalOptions.workingDir, testSuiteFileName, GlobalOptions.mode)
-    TestSuiteRunner testSuiteRunner = new TestSuiteRunner()
-
     if (testSuiteFileName) {
-      // println "HAS FILE"
+      TestSuiteRunner testSuiteRunner = new TestSuiteRunner()
       testSuiteRunner.runTestSuite(testSuiteFileName)
-    } else {
-      testSuiteRunner.discoverAndRunTestSuites()
     }
-
-    testSuiteRunner.printResults()
+    else {
+      println "Legacy?"
+    }
 
   }
 }
