cf629d4d75582e0794ec587d95fd3fb6b9f26294
Pete Mariani <pcmariani@gmail.com>
2024-01-17 10:29:06 -0500

    added src/

--- boomiScriptTest.sh
+++ boomiScriptTest.sh
@@ -3,6 +3,6 @@
 scriptName="boomiScriptTest.groovy"
 workingDir="$(pwd)"
 
-pushd $BOOMI_GROOVY_HOME >/dev/null
-groovy "$BOOMI_GROOVY_HOME"/"$scriptName" --workingDir "$workingDir" "$@"
+pushd "$BOOMI_GROOVY_HOME/src" >/dev/null
+groovy "$BOOMI_GROOVY_HOME/src/$scriptName" --workingDir "$workingDir" "$@"
 popd >/dev/null
