9aa100df59d7276a24477bcca51e90931236a418
Pete Mariani <pcmariani@gmail.com>
2025-02-27 11:41:06 -0500

    changes for some templates

--- templates/_script_json.groovy
+++ templates/_script_json.groovy
@@ -6,9 +6,15 @@ import com.boomi.execution.ExecutionUtil;
 
 logger = ExecutionUtil.getBaseLogger()
 
+// String exampleDynamicProcessProp = ExecutionUtil.getDynamicProcessProperty("DPP_ExampleDPP") ?: ""
+// ExecutionUtil.setDynamicProcessProperty("DPP_ExampleDPP", exampleDynamicProcessProp, false)
+
 for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     InputStream is = dataContext.getStream(i);
     Properties props = dataContext.getProperties(i);
+
+    // String exampleDynamicDocumentProp = props.getProperty("document.dynamic.userdefined.ddp_exampleDdp") ?: ""
+    // props.setProperty("document.dynamic.userdefined.ddp_exampleDdp", exampleDynamicDocumentProp)
 
     def root = new JsonSlurper().parse(is)
 
